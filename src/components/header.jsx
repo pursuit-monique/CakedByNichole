4ce25158dccb0a322a027c8a43bfec7d02c21681
Monique <107360758+pursuit-monique@users.noreply.github.com>
2024-05-12 12:42:06 -0400

    Footer complete

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <>
+            <header className="h-12 w-screen bg-rose-100 fixed top-0 z-50">
+
+            </header>
+        </>
+    )
+}
