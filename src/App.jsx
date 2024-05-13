5b87c18be8d078ad3d1f4f02320e9c45f0978ee0
marciafoster <mfoster9tre@gmail.com>
2024-05-13 15:47:01 -0400

    navbar trial

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,33 @@ import { far } from '@fortawesome/free-regular-svg-icons'
 
 import Index from './pages';
 import Detail from './pages/detail';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import CustomCakes from './pages/CustomCakes';
+import Treats from './pages/Treats';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Header from './components/Header';
+
 
 import './index.css';
 
 function App() {
   return (
     <>
-    <div className="md:flex md:justify-center">
+    <div className="md:flex md:justify-center bg-pink-300">
         <Routes>
-          
+            <Route element={<Header />} path="/" />
             <Route element={<Index />} path="/" />
             <Route element={<Detail />} path="/:id" />
+            <Route element={<Home />} path="/Home" />
+            <Route element={<Shop />} path="/Shop" />
+            <Route element={<CustomCakes />} path="/CustomCakes" />
+            <Route element={<Treats />} path="/Treats" />
+            <Route element={<About />} path="/About" />
+            <Route element={<Blog />} path="/Blog" />
+            
+            
           
           {/* <div className="md:flex md:justify-center">
             <CardSkeleton />
---
+++ src/pages/CustomCakes.jsx
@@ -0,0 +1,11 @@
+
+
+const CustomCakes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CustomCakes;
